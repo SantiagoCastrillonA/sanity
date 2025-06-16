0135e76c1631bb3e4d4b0e76cf6c86f311ddedf5
carlosadso2024 <carlosmario.a@hotmail.com>
2025-06-16 14:54:32 -0500

    iniciando vista login

---
+++ client/src/components/pages/Login.jsx
@@ -0,0 +1,32 @@
+export const LoginPage = () => {
+    return (
+        <div class="bg-blue-200 h-lvh">
+            <div>
+                <img src="" alt="" />
+                <h2>Sanity</h2>
+                <p>Es momento de estar bien</p>
+                <form>
+                    <div>
+                        <input type="email" />
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <input type="password" name="" id="" />
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="rememberSession" />
+                        <label for="rememberSession"></label>
+                        <a href="">¿Olvidó su contrañesa?</a>
+                    </div>
+                    <div>
+                        <button>Iniciar Sesión</button>
+                        <p>¿Primera vez? Registrate o ingresa con</p>
+                        <button>Iniciar sesión con google</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+}
